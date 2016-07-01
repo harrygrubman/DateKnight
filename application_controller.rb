24fa0e3f172a0b13317af6c3b60103f9ba225a3f
Harrison Grubman <harry01px2018@Harrison-Grubman-2.local>
2016-07-01 13:30:27 -0400

    adding sms functionality

--- application_controller.rb
+++ application_controller.rb
@@ -4,6 +4,12 @@ Bundler.require
 
 class ApplicationController < Sinatra::Base
 
+  #begin twilio ruby code
+account_sid ||= ENV["account_sid"]
+auth_token ||= ENV["auth_token"]
+
+#end twilio ruby code
+
   get '/' do
     erb :index
   end
@@ -13,6 +19,23 @@ class ApplicationController < Sinatra::Base
     @yelpURL = yelp(params[:zipCode], params[:category])
     @happycowURL = happycow(params[:zipCode])
     erb :results
+  end
+
+  post '/success' do
+
+		# @message = params[:message] + " --Sent from https://date-knight.herokuapp.com/"
+		# @destination = params[:destination]
+		# @puppy = params[:puppy]
+
+		@client = Twilio::REST::Client.new(account_sid, auth_token) # set up a client to talk to the Twilio REST API
+
+		@client.account.messages.create({
+			:from => '+16692383283 ',
+			:to => '+14752235842',
+			:body => 'this'
+		})
+    puts params
+		erb :success
   end
 
 end
